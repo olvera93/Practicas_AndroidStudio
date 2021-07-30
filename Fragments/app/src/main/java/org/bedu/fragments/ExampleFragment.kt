8b2f36ae824e29927c97b0d0f0352e425abfdd08
Gonzalo Olvera Monroy <olvera9302@outlook.com>
2021-07-29 20:24:24 -0500

    Add ListDetailFragment

--- Fragments/app/src/main/java/org/bedu/fragments/ExampleFragment.kt
+++ Fragments/app/src/main/java/org/bedu/fragments/ExampleFragment.kt
@@ -1,13 +1,27 @@
 package org.bedu.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class ExampleFragment: Fragment() {
 
+
+    override fun onAttach(context: Context) {
+        Log.d("Fragment", "onAttach llamado")
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Fragment", "onCreate llamado")
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,8 +29,65 @@ class ExampleFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_layout, container, false)
+        val root = inflater.inflate(R.layout.fragment_layout, container, false)
+
+        Log.d("Fragment", "onCreateView llamado")
+        val btn = root.findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            val intent = Intent(activity, OtherActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        return root
     }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d("Fragment", "onActivityCreated llamado")
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        Log.d("Fragment", "onViewStateRestored llamado")
+        super.onViewStateRestored(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d("Fragment", "onStart llamado")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("Fragment", "onResume llamado")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("Fragment", "onPause llamado")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("Fragment", "onStop llamado")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("Fragment", "onDestroy llamado")
+        super.onDestroy()
+    }
+
+    override fun onDestroyView() {
+        Log.d("Fragment", "onDestroyView llamado")
+        super.onDestroyView()
+    }
+
+    override fun onDetach() {
+        Log.d("Fragment", "onDetach llamado")
+        super.onDetach()
+    }
+
 
 
 
