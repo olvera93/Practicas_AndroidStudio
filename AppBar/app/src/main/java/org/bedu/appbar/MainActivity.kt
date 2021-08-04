ed5611f7d3786298b77ec0772aa6a81ae9e758aa
Gonzalo Olvera Monroy <olvera9302@outlook.com>
2021-08-03 21:18:03 -0500

    Add AppBar

---
+++ AppBar/app/src/main/java/org/bedu/appbar/MainActivity.kt
@@ -0,0 +1,35 @@
+package org.bedu.appbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val appBar = findViewById<Toolbar>(R.id.app_bar)
+        this.setSupportActionBar(appBar)
+
+        setupDrawer(appBar)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    private fun setupDrawer(toolbar: Toolbar){
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val drawerToggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer)
+    }
+
+
+}
