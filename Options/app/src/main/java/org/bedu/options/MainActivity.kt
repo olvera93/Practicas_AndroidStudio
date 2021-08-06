76d24cae7304c0be4d46e2fd59eb372ee9bc5ab4
Gonzalo Olvera Monroy <olvera9302@outlook.com>
2021-08-05 20:03:42 -0500

    Add Contextual, Popup, Option and AxtionMode

---
+++ Options/app/src/main/java/org/bedu/options/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.bedu.options
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // Pinta el menu en la pantalla
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
