562d768e46e849759b2dc0b2441b904f092a2860
Gonzalo Olvera Monroy <olvera9302@outlook.com>
2021-09-09 21:35:57 -0500

    [ADD-EXERCISE] PhoneCallReceiver

--- BroadcastReceivers/app/src/main/java/org/bedu/broadcastreceivers/MainActivity.kt
+++ BroadcastReceivers/app/src/main/java/org/bedu/broadcastreceivers/MainActivity.kt
@@ -1,10 +1,17 @@
 package org.bedu.broadcastreceivers
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.TelephonyManager
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private val receiverTwo = ReceiverTwo()
     private val airplaneReceiver = AirplaneReceiver()
+    private val phoneCallReceiver = PhoneCallReceiver()
 
     companion object {
         const val NAME = "NAME"
@@ -27,11 +35,74 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.button)
 
         button.setOnClickListener {
-            emit()
+
+            if (!hasPermission()){
+                askPermission()
+            } else {
+                recordCallIfExists()
+            }
+
+
         }
 
-        registerAirplane()
+        //registerAirplane()
+
     }
+
+    private fun hasPermission(): Boolean{
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_PHONE_STATE ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun askPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), 1)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1) {
+
+
+            if (grantResults.isNotEmpty()
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                // Aqui va el código si aceptó el permiso
+            } else {
+                Toast.makeText(
+                    this,
+                    "No puedes acceder sin este permiso",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
+
+    }
+
+    private fun recordCallIfExists(){
+        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+        if (telephonyManager.callState == TelephonyManager.CALL_STATE_OFFHOOK) {
+            Toast.makeText(this, "Grabando llamada", Toast.LENGTH_SHORT).show()
+            registerReceiver(
+                phoneCallReceiver,
+                IntentFilter("android.intent.action.PHONE_STATE")
+            )
+
+        } else {
+            Toast.makeText(this, "No estás en ninguna llamada", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 
     override fun onStart() {
         super.onStart()
@@ -48,7 +119,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(airplaneReceiver)
+        unregisterReceiver(phoneCallReceiver)
     }
 
     private fun registerAirplane() {
@@ -56,6 +127,8 @@ class MainActivity : AppCompatActivity() {
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
         }.also { filter -> registerReceiver(airplaneReceiver, filter) }
     }
+
+
 
     private fun emit() {
         val bundle = Bundle().apply {
---
+++ BroadcastReceivers/app/src/main/java/org/bedu/broadcastreceivers/PhoneCallReceiver.kt
@@ -0,0 +1,13 @@
+package org.bedu.broadcastreceivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class PhoneCallReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+
+    }
+}
