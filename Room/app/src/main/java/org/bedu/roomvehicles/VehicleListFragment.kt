ff2d8c8ca5db0788140169e03870be9f4b33fb34
Gonzalo Olvera Monroy <olvera9302@outlook.com>
2021-09-09 22:12:21 -0500

    [ADD] Content Providers

--- Room/app/src/main/java/org/bedu/roomvehicles/VehicleListFragment.kt
+++ Room/app/src/main/java/org/bedu/roomvehicles/VehicleListFragment.kt
@@ -1,5 +1,7 @@
 package org.bedu.roomvehicles
 
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -11,8 +13,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.bedu.roomvehicles.provider.VehicleProvider
 import org.bedu.roomvehicles.room.Vehicle
 import org.bedu.roomvehicles.room.VehicleDataBase
 import java.util.concurrent.ExecutorService
@@ -28,6 +35,73 @@ class VehicleListFragment : Fragment(), ItemListener {
     private lateinit var addButton: FloatingActionButton
     private lateinit var recyclerVehicle: RecyclerView
     private lateinit var adapter: VehicleAdapter
+
+    private val vehicleArray = mutableListOf<Vehicle>()
+
+    private val loaderCallbacks: LoaderManager.LoaderCallbacks<Cursor> = object : LoaderManager.LoaderCallbacks<Cursor> {
+        override fun onCreateLoader(id: Int, @Nullable args: Bundle?): Loader<Cursor?> {
+            Log.d("Vehicles","holamei")
+            Log.d("Vehicles","${VehicleProvider.URI_VEHICLE}")
+            return CursorLoader(requireContext().applicationContext,
+                Uri.parse("${VehicleProvider.URI_VEHICLE}"),
+                arrayOf(
+                    Vehicle.COLUMN_PK,
+                    Vehicle.COLUMN_BRAND,
+                    Vehicle.COLUMN_MODEL,
+                    Vehicle.COLUMN_PLATES,
+                    Vehicle.COLUMN_WORKING
+                ),
+                null,
+                null,
+                null
+            )
+        }
+
+        override fun onLoadFinished(loader: Loader<Cursor?>, data: Cursor?) {
+
+            Log.d("Vehicles","termina, ${data?.count}")
+
+            data?.apply {
+                // Determine the column index of the column named "word"
+                val pkIndex: Int = getColumnIndex(Vehicle.COLUMN_PK)
+                val brandIndex: Int = getColumnIndex(Vehicle.COLUMN_BRAND)
+                val modelIndex: Int = getColumnIndex(Vehicle.COLUMN_MODEL)
+                val platesIndex: Int = getColumnIndex(Vehicle.COLUMN_PLATES)
+                val workingIndex: Int = getColumnIndex(Vehicle.COLUMN_WORKING)
+
+                while (moveToNext()) {
+                    // Gets the value from the column.
+                    val pk = getInt(pkIndex)
+                    val brand = getString(brandIndex)
+                    val model = getString(modelIndex)
+                    val plates = getString(platesIndex)
+
+                    val vehicle = Vehicle(
+                        id = pk,
+                        brand = brand,
+                        model = model,
+                        platesNumber = plates,
+                        isWorking = true
+                    )
+
+
+                    vehicleArray.add(vehicle)
+                }
+            }
+
+
+
+
+            adapter = VehicleAdapter(vehicleArray, getListener())
+            recyclerVehicle.adapter = adapter
+        }
+
+        override fun onLoaderReset(loader: Loader<Cursor?>) {
+            recyclerVehicle.adapter = null
+        }
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- Room/app/src/main/java/org/bedu/roomvehicles/room/VehicleDao.kt
+++ Room/app/src/main/java/org/bedu/roomvehicles/room/VehicleDao.kt
@@ -1,32 +1,35 @@
 package org.bedu.roomvehicles.room
 
+import android.database.Cursor
 import androidx.room.*
 
 @Dao
 interface VehicleDao {
 
     @Insert
-    fun insertVehicle(vehicle: Vehicle)
+    fun insertAll(vehicle: List<Vehicle>)
+
+    @Insert
+    fun insertVehicle(vehicle: Vehicle): Long
 
     @Update
-    fun updateVehicle(vehicle: Vehicle)
+    fun updateVehicle(vehicle: Vehicle): Int
 
     @Delete
-    fun deleteVehicle(vehicle: Vehicle)
+    fun removeVehicle(vehicle: Vehicle)
 
-    @Query("DELETE FROM Vehicle WHERE id=:id")
-    fun removeVehicleById(id: Int)
+    @Query("DELETE FROM ${Vehicle.TABLE_NAME} WHERE ${Vehicle.COLUMN_PK}=:id")
+    fun removeVehicleById(id: Int): Int
 
     @Delete
-    fun removeVehicles(vararg vehicles: Vehicle) // Argumentos
+    fun removeVehicles(vararg vehicles: Vehicle)
 
-    @Query("SELECT * FROM Vehicle")
-    fun getVehicles(): List<Vehicle>
+    @Query("SELECT * FROM ${Vehicle.TABLE_NAME}")
+    fun getVehicles(): Cursor
 
-    @Query("SELECT * FROM Vehicle WHERE brand = :brand")
-    fun getVehiclesByBrand(brand: String): List<Vehicle>
+    @Query("SELECT * FROM ${Vehicle.TABLE_NAME} WHERE ${Vehicle.COLUMN_PK} = :id")
+    fun getVehicleById(id: Int): Cursor
 
-    @Query("SELECT * FROM Vehicle WHERE id = :id")
-    fun getVehicleById(id: Int): Vehicle
-
+    @Query("SELECT * FROM ${Vehicle.TABLE_NAME} WHERE plates_number = :platesNumber")
+    fun getVehicleByPlates(platesNumber: String) : Cursor
 }
