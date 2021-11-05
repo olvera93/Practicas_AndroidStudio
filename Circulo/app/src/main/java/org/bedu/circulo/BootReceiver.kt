10dc955d1ec12ac8e051136a646f724e4a8b02e1
Gonzalo Olvera Monroy <olvera9302@outlook.com>
2021-11-04 21:24:25 -0600

    [ADD] Circulo (Notifications)

---
+++ Circulo/app/src/main/java/org/bedu/circulo/BootReceiver.kt
@@ -0,0 +1,18 @@
+package org.bedu.circulo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+        if (action != null) {
+            if (action == Intent.ACTION_BOOT_COMPLETED) {
+                AlarmReceiver().setAlarm(context, true)
+            }
+        }
+
+    }
+}
