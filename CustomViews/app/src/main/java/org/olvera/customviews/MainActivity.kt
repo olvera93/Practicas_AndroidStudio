257719e6ad35931f263334da63da589dc980b5e3
Gonzalo Olvera Monroy <olvera9302@outlook.com>
2021-12-07 14:20:57 -0600

    [ADD] CustomViews

---
+++ CustomViews/app/src/main/java/org/olvera/customviews/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.olvera.customviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.olvera.customviews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+    }
+}
