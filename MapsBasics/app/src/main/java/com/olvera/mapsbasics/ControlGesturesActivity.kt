54e5292ff4fc31c73115352c90b6c817ade50e7f
olvera93 <olvera9302@outlook.com>
2022-11-03 17:30:34 -0600

    [ADD] Padding

--- MapsBasics/app/src/main/java/com/olvera/mapsbasics/ControlGesturesActivity.kt
+++ MapsBasics/app/src/main/java/com/olvera/mapsbasics/ControlGesturesActivity.kt
@@ -7,17 +7,24 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.MarkerOptions
+import com.olvera.mapsbasics.Utils.dp
+import com.olvera.mapsbasics.databinding.ActivityFirstMapBinding
 
 class ControlGesturesActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var binding: ActivityFirstMapBinding
     private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_map)
+
+        binding = ActivityFirstMapBinding.inflate(layoutInflater)
+        Utils.binding = binding
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        setContentView(binding.root)
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -27,8 +34,15 @@ class ControlGesturesActivity : AppCompatActivity(), OnMapReadyCallback {
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(Locations.medillin, 12f))
 
         map.uiSettings.apply {
-            isMyLocationButtonEnabled = true
+            //isMyLocationButtonEnabled = true
+            isZoomControlsEnabled = true
+            isCompassEnabled = false
+            isMapToolbarEnabled = true
+            isRotateGesturesEnabled = false
+            isZoomGesturesEnabled = false
         }
+
+        map.setPadding(0, 0, 0, dp(64))
 
     }
 }
